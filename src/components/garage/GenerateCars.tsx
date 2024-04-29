56ac58820fa7c22a07ae8a3c50a25e13b4650b7a
Ana Arevadze <arevadzeana10@gmail.com>
2024-04-29 22:49:56 +0400

    modified:   src/components/garage/GenerateCars.tsx 	modified:   src/components/garage/dataForRandomCarGenerator.tsx

--- src/components/garage/GenerateCars.tsx
+++ src/components/garage/GenerateCars.tsx
@@ -1,28 +1,33 @@
-import { RandomCars, carBrands, colors } from "./dataForRandomCarGenerator";
+import {
+  RandomCars,
+  carBrands,
+  colors,
+  carModels,
+} from "./dataForRandomCarGenerator";
 
 interface GenerateCarsProps {
   onGenerateCars: (cars: RandomCars[]) => void;
 }
 
 function GenerateCars({ onGenerateCars }: GenerateCarsProps) {
-  function generateCars() {
+  const generateCars = () => {
     const cars: RandomCars[] = [];
 
     for (let i = 0; i < 100; i++) {
+      const randomBrandIndex = Math.floor(Math.random() * carBrands.length);
+      const randomModelIndex = Math.floor(Math.random() * carModels.length);
+      const randomColorIndex = Math.floor(Math.random() * colors.length);
+
       const carData: RandomCars = {
         id: i + 1,
-        name: getRandomCar(carBrands),
-        color: getRandomCar(colors),
+        name: `${carBrands[randomBrandIndex]} ${carModels[randomModelIndex]}`,
+        color: colors[randomColorIndex],
       };
       cars.push(carData);
     }
 
     onGenerateCars(cars);
-  }
-
-  function getRandomCar(arr: string[]) {
-    return arr[Math.floor(Math.random() * arr.length)];
-  }
+  };
 
   return (
     <>
